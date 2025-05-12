f4ddfd707adf757bda32e6db7da24e701c182ce6
SilvesterSpath <spath.sz@gmail.com>
2025-05-12 06:58:07 +0200

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,41 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../img/logo.png';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 const Navbar = () => {
   return (
     <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between items-center'>
       <Link href='/'>
         <Image src={logo} alt='SilvesterPress' width={32} height={32} />
-      </Link>
+      </Link>{' '}
+      <DropdownMenu>
+        <DropdownMenuTrigger className='focus:outline-none'>
+          {' '}
+          <Avatar>
+            <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+            <AvatarFallback className='text-black'>SP</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href='/profile'>Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href='/auth'>Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
