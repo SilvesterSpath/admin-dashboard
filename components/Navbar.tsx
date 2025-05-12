eb59806f291b4ff9763ee5490cddacbaf098af0d
SilvesterSpath <spath.sz@gmail.com>
2025-05-12 06:44:11 +0200

    avatar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../img/logo.png';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between items-center'>
+      <Link href='/'>
+        <Image src={logo} alt='SilvesterPress' width={32} height={32} />
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
