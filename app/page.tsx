fe523bf84e7eb20dcb50a17fa58cbb17f980aa0e
SilvesterSpath <spath.sz@gmail.com>
2025-05-16 06:27:35 +0200

    DashboardCard init

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import DashboardCard from '@/components/dashboard/DashboardCard';
+
 export default function Home() {
   return (
     <>
       <div className='text-2xl'>Dashboard</div>
+      <DashboardCard />
     </>
   );
 }
