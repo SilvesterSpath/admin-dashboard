4c5dc45c55cce0441c3e2ebe3fea68f1e32b908a
SilvesterSpath <spath.sz@gmail.com>
2025-05-16 06:22:31 +0200

    Sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -30,14 +30,32 @@ const SideBar = () => {
             <LayoutDashboard className='mr-2 h-4 w-4' />
             <Link href='/'>Dashboard</Link>
           </CommandItem>
-          <CommandItem>Search Emoji</CommandItem>
-          <CommandItem>Calculator</CommandItem>
+          <CommandItem>
+            <Newspaper className='mr-2 h-4 w-4' />
+            <Link href='/posts'>Posts</Link>
+          </CommandItem>
+          <CommandItem>
+            <Folders className='mr-2 h-4 w-4' />
+            <Link href='#'>Categories</Link>
+          </CommandItem>
         </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading='Settings'>
-          <CommandItem>Profile</CommandItem>
-          <CommandItem>Billing</CommandItem>
-          <CommandItem>Settings</CommandItem>
+          <CommandItem>
+            <User className='mr-2 h-4 w-4' />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className='mr-2 h-4 w-4' />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className='mr-2 h-4 w-4' />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
         </CommandGroup>
       </CommandList>
     </Command>
